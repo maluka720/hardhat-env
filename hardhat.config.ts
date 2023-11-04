9510e96653c5d37037773cac6b35205d23c1c1c3
majun <majun@teamind.co>
2023-11-04 22:37:59 +0800

    init hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://localhost:8545",
+    },
+  },
 };
 
 export default config;
